86607f716b4646ebac0e207b54f277892a1befe7
BlessedRebuS <patrick.difazio@studio.unibo.it>
2022-11-21 01:33:19 +0100

    da modificare entrypoint

---
+++ src/facial_signup_button.py
@@ -0,0 +1,42 @@
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+import time
+import cv2
+import sys
+import curses
+
+def button_callback(channel):
+    user = sys.argv[1]
+    print("Button was pushed!")
+    print(user)
+
+    # intialize the webcam and pass a constant which is 0
+    cam = cv2.VideoCapture(0)
+
+    # title of the app
+   # cv2.namedWindow('python webcam screenshot app')
+
+    # while loop
+    while True:
+        # intializing the frame, ret
+        ret, frame = cam.read()
+        # if statement
+        #if not ret:
+        #    print('failed to grab frame')
+        #    break
+        img_name = f'../faces/{user}.jpg'
+        cv2.imwrite(img_name, frame)
+        print("done")
+        break       
+
+    # release the camera
+    cam.release()
+
+GPIO.setwarnings(False) # Ignore warning for now
+
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
+#ciclo infinito per aspettare le callback, da modificare ENTRYPOINT
+time.sleep(100)
+#message = input("Press enter to quit\n\n") # Run until someone presses enter
+GPIO.cleanup() # Clean up
