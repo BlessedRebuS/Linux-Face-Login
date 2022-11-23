c5f144b9e7b1bd70cf6dad104d6f536688cd9fbf
BlessedRebuS <patrick.difa@gmail.com>
2022-11-23 16:45:22 +0100

    added raspberry configuration

--- src/facial_signup_button.py
+++ src/facial_signup_button.py
@@ -84,7 +84,6 @@ x = 0
 # Load default font.
 font = ImageFont.load_default()
 GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
 #ciclo infinito per aspettare le callback, da modificare ENTRYPOINT
